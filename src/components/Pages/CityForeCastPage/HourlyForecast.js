d5e89daa0bccdcfcc9eee15657a4f8b14d0504bd
OrenRorberg <orenrorberg1@gmail.com>
2022-02-08 19:48:26 +0200

    Create HourlyForecast.js

---
+++ src/components/Pages/CityForeCastPage/HourlyForecast.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HourlyForecast =(props)=> {
+  return (<div className="" >
+
+  </div>);
+}
+
+HourlyForecast.propTypes = {};
+
+export default HourlyForecast;
