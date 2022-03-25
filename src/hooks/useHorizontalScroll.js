359bb2954f3df3c01df7e905c2265a612c31f2d6
drorreuven31 <drorreuven31@gmail.com>
2022-03-25 19:53:53 +0300

    fixing scrolling issue - Final Commit

--- src/hooks/useHorizontalScroll.js
+++ src/hooks/useHorizontalScroll.js
@@ -1,21 +1,39 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect } from "react";
 
 export function useHorizontalScroll() {
-  const elRef = useRef()
+  const elRef = useRef();
   useEffect(() => {
-    const el = elRef.current
+    const el = elRef.current;
     if (el) {
       const onWheel = (e) => {
-        if (e.deltaY == 0) return
-        e.preventDefault()
+        if (e.deltaY == 0) return;
+        e.preventDefault();
         el.scrollTo({
-          left: el.scrollLeft + e.deltaY*1.4,
-          behavior: 'smooth',
-        })
-      }
-      el.addEventListener('wheel', onWheel)
-      return () => el.removeEventListener('wheel', onWheel)
+          left: el.scrollLeft + e.deltaY * 1.4,
+          behavior: "smooth",
+        });
+      };
+      el.addEventListener("wheel", onWheel);
+      return () => el.removeEventListener("wheel", onWheel);
     }
-  }, [])
-  return elRef
+  }, []);
+
+  useEffect(() => {
+    const el = elRef.current;
+    if (el) {
+      const onTouch = (ev) => {
+        debugger;
+        console.log("hey");
+        ev.stopPropagation();
+      };
+      el.addEventListener("touchmove", onTouch);
+      return () => {
+        console.log("bye");
+
+        el.removeEventListener("touchmove", onTouch);
+      };
+    }
+  }, []);
+
+  return elRef;
 }
